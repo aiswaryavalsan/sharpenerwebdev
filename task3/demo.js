98cf1196b3cadc9293b2839b16a0c8e0838e3488
Aiswarya <aiswaryavalsan@gmail.com>
2022-12-11 16:30:32 +0530

    task3

---
+++ task3/demo.js
@@ -0,0 +1,7 @@
+let item =document.getElementsByTagName('li');
+for(let i=0;i<item.length;i++){
+    item[i].style.backgroundColor='yellow';
+    item[i].style.color='red';
+}
+item[4].style.backgroundColor='red';
+item[4].style.color='yellow';
